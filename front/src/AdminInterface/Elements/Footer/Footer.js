8f3535e98a5995c30e7f5c50d4976770dffe1fbf
Abhishek Gupta <abhishek@braintechnosys.com>
2020-01-01 17:01:54 +0530

    added new pages

---
+++ front/src/AdminInterface/Elements/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Footer = props => {
+    return (
+        <footer className="main-footer">
+            <div className="pull-right hidden-xs">
+                <b>Version</b> 2.4.0
+            </div>
+            <strong>Copyright &copy; 2014-2016 <a href="https://adminlte.io">Almsaeed Studio</a>.</strong> All rights
+            reserved.
+        </footer>
+    )
+}
+
+export default Footer
